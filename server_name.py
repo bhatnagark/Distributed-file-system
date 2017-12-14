0ccdadcd15d997d2d7e70ff90f7dec0e0341d9c2
Kshitij Bhatnagar <32340359+bhatnagark@users.noreply.github.com>
2017-12-14 20:37:38 +0000

    server_name file with update name function

--- server_name.py
+++ server_name.py
@@ -30,15 +30,75 @@ class NameService:
         if directorypath in _name:
             return _name[directorypath]
 
-        raise web.notfound('No file server serve this file.')
+        raise web.notfound('No file server has this file.')
 
 
     def POST(self, directorypath):
-        """See _update (with add=True)."""
-
+        
         return _update(str(directorypath))
 
     def DELETE(self, directorypath):
-        """See _update (with add=False)."""
-
+        
         return _update(str(directorypath), False)
+
+
+def _update(directorypath, add=True):
+    
+    web.header('Content-Type', 'text/plain; charset=UTF-8')
+    j = web.input()
+
+    if 'srv' not in j:
+        raise web.badrequest()
+
+    srv = j['srv']
+
+    if directorypath == '/':
+        if 'dirs' not in j:
+            raise web.badrequest()
+
+        for directorypath in j['dirs'].split('\n'):
+            if not directorypath:
+                continue
+
+            try:
+                _update_names(directorypath, srv, add)
+            except ValueError as e:
+                logging.exception(e)
+
+    else:
+        try:
+            _update_names(directorypath, srv, add)
+        except ValueError as e:
+            logging.exception(e)
+
+
+    return 'OK' 
+
+
+def _update_names(directorypath, srv, add=True):
+    
+    if directorypath[-1] == '/':
+        directorypath = os.path.directoryname(directorypath)
+
+    if add:
+        logging.info('Update directory %s on %s.', directorypath, srv)
+        _name[directorypath] = srv
+
+    elif directorypath in _name:
+        logging.info('Remove directory %s on %s.', directorypath, srv)
+        del _name[directorypath]
+
+    else:
+        raise ValueError('%s wasn\'t not deleted, because it wasn\'t'
+                         ' in the dictionnary/database.' % directorypath)
+
+
+_config = {
+            'dbfile': 'names.db',
+         }
+
+logging.info('Loading config file nameserver.dfs.json.')
+utils.load_config(_config, 'nameserver.dfs.json')
+_name = shelve.open(_config['dbfile'])
+
+atexit.register(lambda: _name.close())
