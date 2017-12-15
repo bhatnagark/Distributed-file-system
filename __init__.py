6132c94ef3a4fcc51cf77ae8f74bfaaff22ae498
Kshitij Bhatnagar <32340359+bhatnagark@users.noreply.github.com>
2017-12-15 23:27:52 +0000

    init file for login

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: kshitijbhatnagar
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
